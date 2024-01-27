cdea76d4cee48c8b50fc6d56488235798f8eedaa
Jonathan May <jonafmay@gmail.com>
2024-01-27 21:46:35 +0000

    Adding previously made scenes: stroke the injury

--- scenes/game/levels/stroke_the_injury/stroke_the_injury.gd
+++ scenes/game/levels/stroke_the_injury/stroke_the_injury.gd
@@ -6,7 +6,7 @@ var stroke_timer = 0.0
 var is_playing_nod = false
 var is_playing_shake = false
 @export var victory_time = 3.0
-@onready var anim_player = $Area2D/CollisionPolygon2D2/InjuryBody/AnimationPlayer
+@onready var anim_player = $Area2D/CollisionPolygon2D/InjuryBody/AnimationPlayer
 
 
 # Called when the node enters the scene tree for the first time.
@@ -23,10 +23,12 @@ func _process(delta):
 
 			_play_shake()
 	elif  Input.get_last_mouse_velocity().length() == 0:
+		#print("STOPPED")
 		is_stroking = false
 	else: 
 		is_stroking = true
 	if Input.is_action_pressed("Click") && is_stroking && is_hover_injury:
+		print("Stroking")
 		stroke_timer += delta
 		print(stroke_timer)
 		_play_nod()
@@ -38,24 +40,27 @@ func _process(delta):
 
 func _on_area_2d_mouse_entered():
 	is_hover_injury = true
+	print("hover")
 
 
 func _on_area_2d_mouse_exited():
+	print("stop hover")
 	is_hover_injury = false
 	
 func _play_shake():
 	if is_playing_shake == false:
 		is_playing_shake = true
-		anim_player.speed_scale = 20
+		anim_player.speed_scale = 10
 		anim_player.play("Shake")
 
 func _play_nod():
 	if is_playing_nod == false:
 		is_playing_nod = true
-		anim_player.speed_scale = 2
+		anim_player.speed_scale = 1
 		anim_player.play("Nod")
 
 
 func _on_animation_player_animation_finished(anim_name):
 	is_playing_nod = false
 	is_playing_shake = false
+
