193798e096482ecd9a368fe1b00c5b01428ff9cd
Egor Grigorev <egor.grigorev@gmail.com>
2024-01-27 21:56:15 +0000

    full merge complete

--- global/scene_handler.gd
+++ global/scene_handler.gd
@@ -1,21 +1,5 @@
 extends Node
 class_name SceneHandler
- 
-# A collection of scenes in the game. Scenes are added through the Inspector panel
-#@export var Scenes : Dictionary = {
-	#"Main" : "res://scenes/main.tscn",
-	#"MainMenu" : "res://scenes/menu/main_menu.tscn",
-	#"TransitionScene" : "res://scenes/game/transition_scene/transition_scene.tscn",
-	#"FightTheDrawer" : "res://scenes/game/levels/fight_the_drawer/fight_the_drawer.tscn",
-	#"AccumulateTheFriendship" : "res://scenes/game/levels/accumulate_the_friendship/accumulate_the_friendship.tscn",
-	#"ThrowThePoet" : "res://scenes/game/levels/throw_the_poet/throw_the_poet.tscn",
-	#"StealTheBird" : "res://scenes/game/levels/steal_the_bird/steal_the_bird.tscn",
-	#"StrokeTheInjury" : "res://scenes/game/levels/stroke_the_injury/stroke_the_injury.tscn",
-#}
-
-
-
-#@export var Scenes : Dictionary = Global.LevelScenes
 
 #@export var TransitionScene : Dictionary = {
 	#StringName("TransitionScene") : StringName("res://scenes/game/transition_scene/transition_scene.tscn")
