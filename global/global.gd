37a2a560ecd623f5cbf432a4f0d342174c991707
Egor Grigorev <egor.grigorev@gmail.com>
2024-01-27 21:49:22 +0000

    "moving all non-changeable variables to global"

--- global/global.gd
+++ global/global.gd
@@ -10,12 +10,14 @@ var high_scores : Array;
 @export var difficulty : int;
 @export var lives : int;
 @export var LevelScenes : Dictionary = {
-	StringName("Main") : StringName("res://scenes/main.tscn"),
-	StringName("MainMenu") : StringName("res://scenes/menu/main_menu.tscn"),
-	StringName("TransitionScene") : StringName("res://scenes/game/transition_scene/transition_scene.tscn"),
-	StringName("FightTheDrawer") : StringName("res://scenes/game/levels/fight_the_drawer/fight_the_drawer.tscn"),
-	StringName("AccumulateTheFriendship") : StringName("res://scenes/game/levels/accumulate_the_friendship/accumulate_the_friendship.tscn"),
-	StringName("ThrowThePoet") : StringName("res://scenes/game/levels/throw_the_poet/throw_the_poet.tscn")
+	"Main" : "res://scenes/main.tscn",
+	"MainMenu" : "res://scenes/menu/main_menu.tscn",
+	"TransitionScene" : "res://scenes/game/transition_scene/transition_scene.tscn",
+	"FightTheDrawer" : "res://scenes/game/levels/fight_the_drawer/fight_the_drawer.tscn",
+	"AccumulateTheFriendship" : "res://scenes/game/levels/accumulate_the_friendship/accumulate_the_friendship.tscn",
+	"ThrowThePoet" : "res://scenes/game/levels/throw_the_poet/throw_the_poet.tscn",
+	"StealTheBird" : "res://scenes/game/levels/steal_the_bird/steal_the_bird.tscn",
+	"StrokeTheInjury" : "res://scenes/game/levels/stroke_the_injury/stroke_the_injury.tscn",
 }
 
 @export var LevelArray : Array;
--- global/scene_handler.gd
+++ global/scene_handler.gd
@@ -2,16 +2,16 @@ extends Node
 class_name SceneHandler
  
 # A collection of scenes in the game. Scenes are added through the Inspector panel
-@export var Scenes : Dictionary = {
-	"Main" : "res://scenes/main.tscn",
-	"MainMenu" : "res://scenes/menu/main_menu.tscn",
-	"TransitionScene" : "res://scenes/game/transition_scene/transition_scene.tscn",
-	"FightTheDrawer" : "res://scenes/game/levels/fight_the_drawer/fight_the_drawer.tscn",
-	"AccumulateTheFriendship" : "res://scenes/game/levels/accumulate_the_friendship/accumulate_the_friendship.tscn",
-	"ThrowThePoet" : "res://scenes/game/levels/throw_the_poet/throw_the_poet.tscn",
-	"StealTheBird" : "res://scenes/game/levels/steal_the_bird/steal_the_bird.tscn",
-	"StrokeTheInjury" : "res://scenes/game/levels/stroke_the_injury/stroke_the_injury.tscn",
-}
+#@export var Scenes : Dictionary = {
+	#"Main" : "res://scenes/main.tscn",
+	#"MainMenu" : "res://scenes/menu/main_menu.tscn",
+	#"TransitionScene" : "res://scenes/game/transition_scene/transition_scene.tscn",
+	#"FightTheDrawer" : "res://scenes/game/levels/fight_the_drawer/fight_the_drawer.tscn",
+	#"AccumulateTheFriendship" : "res://scenes/game/levels/accumulate_the_friendship/accumulate_the_friendship.tscn",
+	#"ThrowThePoet" : "res://scenes/game/levels/throw_the_poet/throw_the_poet.tscn",
+	#"StealTheBird" : "res://scenes/game/levels/steal_the_bird/steal_the_bird.tscn",
+	#"StrokeTheInjury" : "res://scenes/game/levels/stroke_the_injury/stroke_the_injury.tscn",
+#}
 
 
 
@@ -28,7 +28,7 @@ var m_CurrentSceneAlias : String = ""
 func _ready() -> void:
 	var mainScene : String = ProjectSettings.get_setting("application/run/main_scene")
 	#print(mainScene)
-	m_CurrentSceneAlias = Scenes.find_key(mainScene)
+	m_CurrentSceneAlias = Global.LevelScenes.find_key(mainScene)
 
 func PickScene(LevelArray : Array):
 	randomize()
@@ -41,7 +41,7 @@ func PickScene(LevelArray : Array):
 # Description: Switch to the requested scene based on its alias
 # Parameter sceneAlias: The scene alias of the scene to switch to
 func SwitchScene(sceneAlias : String) -> void:
-	SceneManager.change_scene(Scenes[sceneAlias])
+	SceneManager.change_scene(Global.LevelScenes[sceneAlias])
 
 # Description: Restart the current scene
 func RestartScene() -> void:
@@ -53,7 +53,7 @@ func QuitGame() -> void:
 
 # Description: Return the number of scenes in the collection
 func GetSceneCount() -> int:
-	return Scenes.size()
+	return Global.LevelScenes.size()
 	 
 # Description: Returns the alias of the current scene
 func GetCurrentSceneAlias() -> String:
@@ -63,9 +63,9 @@ func GetCurrentSceneAlias() -> String:
 # Parameter sceneAlias: The alias used for finding the scene in the collection
 # Parameter scenePath: The full path to the scene file in the file system
 func AddScene(sceneAlias : String, scenePath : String) -> void:
-	Scenes[sceneAlias] = scenePath
+	Global.LevelScenes[sceneAlias] = scenePath
 	 
 # Description: Remove an existing scene from the scene collection
 # Parameter sceneAlias: The scene alias of the scene to remove from the collection
 func RemoveScene(sceneAlias : String) -> void:
-	Scenes.erase(sceneAlias)
+	Global.LevelScenes.erase(sceneAlias)
