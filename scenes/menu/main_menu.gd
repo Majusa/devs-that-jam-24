2ca0940899ca7d5a59a0fbd9d7c58e23f0eba7b0
Egor Grigorev <egor.grigorev@gmail.com>
2024-01-28 16:44:43 +0000

    "ungabunga"

--- scenes/menu/main_menu.gd
+++ scenes/menu/main_menu.gd
@@ -14,3 +14,13 @@ func _on_quit_pressed():
 func _on_test_random_pressed():
 	SceneHandlage.PickScene(Global.LevelArray)
 	pass # Replace with function body.
+
+
+func _on_play_input_event(viewport, event, shape_idx):
+	if Input.is_action_just_pressed("Click"):
+		SceneHandlage.SwitchScene("Transition_Scene")
+
+func _on_quit_input_event(viewport, event, shape_idx):
+	if Input.is_action_just_pressed("Click"):
+		SceneHandlage.QuitGame()
+	
